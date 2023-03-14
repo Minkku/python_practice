b84db8cb896dfafd5473af23d4045519307fc940
minkyu_lee <0106995@naver.com>
2023-03-14 14:59:57 +0900

    Initialized django

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud_py',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-ctgbu&lxd-5s!ass+*q4*b8&i=l11-o_t*8snk4s8oryn7opb0'
